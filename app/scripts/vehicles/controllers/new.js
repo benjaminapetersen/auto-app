928e50b3446a0ede3e696ef057224f39b8ab0bf9
Jonathan Sowattana Yiv <JonathanYiv@DragonMac.local>
2015-07-16 00:04:56 -0400

    WIP: saving a vehicle

--- app/scripts/vehicles/controllers/new.js
+++ app/scripts/vehicles/controllers/new.js
@@ -1,13 +1,44 @@
+// I don't understand this js file and how two-way binding works.
+
 'use strict';
 
+// I guess this line refers to the vehicles.js file,
 angular.module('auto.vehicles')
 	.controller('auto.vehicles.vehicle.new', [
 		'$log',
 		'$scope',
-		function($log, $scope) {
+    'vehicles',
+
+		function($log, $scope, vehicles) {
 			$log.log('/vehicles/new');
 
+
+      // When using ng-model in the view, it will
+      var vehicle = {
+        year: '',
+        make: '',
+        model: '',
+        color: '',
+      }
+
 			angular.extend($scope, {
-				// pass to view
+
+        vehicle: vehicle,
+
+				create: function() {
+
+          vehicles.create(vehicle);
+
+        }
+
 			});
 		}]);
+
+
+// Services <-> Controller (JS) <-> Views (HTML)
+// Button .save -> vehicle service.save(vehicle)
+
+
+
+// localStorage.setItem('user_id_vehicles', {});
+// [{},{},{}] <-- array of vehicles
--- app/scripts/vehicles/services/vehicles.js
+++ app/scripts/vehicles/services/vehicles.js
@@ -7,14 +7,14 @@ angular.module('auto.vehicles')
 			$log.log('auto.vehicles [service]');
 
 			// Thinking about what data to track for a vehicle
-			// Initially we don't need much, but it may be 
-			// beneficial to allow a user to fill in as much 
-			// information as desired 
+			// Initially we don't need much, but it may be
+			// beneficial to allow a user to fill in as much
+			// information as desired
 			// - extended info should be added in a detail interface
 			// - ensure initial car input is as simple as possible
 			var vehicleExample = {
 				id: 100,
-				year: '2008',	
+				year: '2008',
 				make: 'Toyota', 				// Honda, Toyota, etc
 				model: 'Sienna',				// Civic, Sienna, etc
 				style: 'LE',	 				// CE, LE, XLE, etc
@@ -29,7 +29,7 @@ angular.module('auto.vehicles')
 					{miles: '87431', date: 1435278650422},
 					{miles: '86750', date: 1435278045622},
 					{miles: '85901', date: 1435277440822}
-				], 						
+				],
 				// typical sellable components to a car...
 				features: ['ac', 'power windows', 'cruise control', 'am/fm radio', 'cd', 'mp3', 'etc'],
 				// specific engineering of vehicle
@@ -46,15 +46,16 @@ angular.module('auto.vehicles')
 						vehicle.id = _.uniqueId('vehicle_');
 						return vehicle;
 					});
- 
+
 
 			// for prototyping, simply using CRUD as an API model
 			// this will probably not work long term, as the vehicles
-			// list in memory, vs on disk or a remote store will be 
+			// list in memory, vs on disk or a remote store will be
 			// different.
 			return {
-				create: function() {
-
+				create: function(vehicle) {
+					// Just a test to see if I can retrieve the vehicle from the view.
+					console.log("vehicle is ", vehicle);
 				},
 				read: function(vid) {
 					console.log('todo: return list of vehicles if no :vid provided');
@@ -66,12 +67,12 @@ angular.module('auto.vehicles')
 
 				},
 				getAll: function() {
-					// simulate an http request by returning a 
-					// promise for an array of 10 cars, which 
+					// simulate an http request by returning a
+					// promise for an array of 10 cars, which
 					// are all replicas of the above car.
 					return $.when(vehicles);
 				},
-				get: function(vid) { 
+				get: function(vid) {
 					return $.when(_.find(vehicles, function(vehicle) {
 						return vehicle.id == vid;
 					}));
